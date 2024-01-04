6baf606aef9a23637196c0b624bb6b288d045620
Joerg, Andreas <andreas.joerg@nuernberger.de>
2024-01-04 10:19:44 +0100

    HttpRouting read response with charset

---
+++ src/test/java/de/joergdev/mosy/backend/util/HttpRoutingTest.java
@@ -0,0 +1,58 @@
+package de.joergdev.mosy.backend.util;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.MultivaluedMap;
+import org.junit.Test;
+
+public class HttpRoutingTest
+{
+  @Test
+  public void testGetCharsetFromHeadersMultivaluedMap()
+  {
+    MultivaluedMap<String, String> multiMap = new MultivaluedHashMap<String, String>();
+    multiMap.put("charset", Arrays.asList("1"));
+
+    assertEquals("1", HttpRouting.getCharsetFromHeaders(multiMap));
+
+    multiMap = new MultivaluedHashMap<String, String>();
+    multiMap.put("Content-Type", Arrays.asList("[text/xml;charset=2]"));
+
+    assertEquals("2", HttpRouting.getCharsetFromHeaders(multiMap));
+
+    assertNull(HttpRouting.getCharsetFromHeaders(new MultivaluedHashMap<String, String>()));
+  }
+
+  @Test
+  public void testGetCharsetFromHeadersMapWithListValues()
+  {
+    Map<String, List<String>> map = new HashMap<>();
+    map.put("CHARSET", Arrays.asList("1"));
+
+    assertEquals("1", HttpRouting.getCharsetFromHeaders(map));
+
+    map = new HashMap<>();
+    map.put("Content-Type", Arrays.asList("[text/xml;charset=2]"));
+
+    assertEquals("2", HttpRouting.getCharsetFromHeaders(map));
+  }
+
+  @Test
+  public void testGetCharsetFromHeadersFlatMap()
+  {
+    Map<String, String> map = new HashMap<>();
+    map.put("charset", "1");
+
+    assertEquals("1", HttpRouting.getCharsetFromHeaders(map));
+
+    map = new HashMap<>();
+    map.put("Content-Type", "[text/xml;charset=2]");
+
+    assertEquals("2", HttpRouting.getCharsetFromHeaders(map));
+  }
+}
